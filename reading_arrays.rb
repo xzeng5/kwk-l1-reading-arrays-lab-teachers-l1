bc84d4968cd1aa6564345a9a7830ba4664765269
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-14 22:00:57 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -29,6 +29,7 @@ end
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
+  STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
